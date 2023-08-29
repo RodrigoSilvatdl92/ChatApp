592fd79fb85575ab6a98027ebb48e7346345e618
RodrigoSilvatdl92 <115164671+RodrigoSilvatdl92@users.noreply.github.com>
2023-08-29 09:33:15 +0100

    final version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
   return (
     <div className="w-screen h-screen fixed top-0 left-0 flex items-center justify-center bg-gray-500 px-2">
       <div className="w-full md:w-[800px] rounded-2xl bg-white mx-2 mb-10">
-        <div className="flex flex-col md:flex-row  h-[600px] xl:mb-0  xl:h-[600px] w-full">
+        <div className="flex flex-col md:flex-row  h-[600px] xl:mb-0  xl:h-[700px] w-full">
           <div className="flex flex-col w-full  md:w-[40%] md:h-full">
             <div className="flex items-center justify-center h-[60px] md:flex md:items-center md:justify-center outline-none">
               <Search />
